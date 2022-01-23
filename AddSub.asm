0b7dc18c9f8ab0deb7f652580af7541b79e699ac
振輔 黃 <chenfukye@gmail.com>
2022-01-23 22:20:43 +0800

    First change

--- AddSub.asm
+++ AddSub.asm
@@ -9,21 +9,15 @@ ExitProcess proto,dwExitCode:dword
 INCLUDE Irvine32.inc
 
 .data 
-val1 DWORD 1
-val2 DWORD 2
-val3 DWORD 4
-sum  DWORD 8
+val1 DWORD 10
 
 .code
 main proc
-	mov		eax,val1			; start eax with  1
-	mov		ebx,val2			; start ebx with  2
-	mov		ecx,val3			; start ecx with  4
-	mov		edx,sum				; start edx with  8
+	mov		eax,val1			; start eax with  10
 
-	sub		edx,eax				; edx subtract eax 8-1=7
-	sub		edx,ebx				; edx subtract eax 7-2=5
-	sub		edx,ecx				; edx subtract eax 5-4=1
+	sub		eax,1				; subtract 1
+	sub		eax,2				; subtract 2
+	sub		eax,4				; subtract 4
 
 	call	DumpRegs			; display the registers
 

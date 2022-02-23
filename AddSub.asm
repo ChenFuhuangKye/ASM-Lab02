5ea6cda34eccc2f0490c0a19449727b43c038e46
振輔 黃 <chenfukye@gmail.com>
2022-02-23 09:21:59 +0800

    First change

--- AddSub.asm
+++ AddSub.asm
@@ -9,18 +9,37 @@ ExitProcess proto,dwExitCode:dword
 INCLUDE Irvine32.inc
 
 .data 
-val1 DWORD 10
+  FIBNum    DWORD 50 DUP(?)
+  vla       DWORD ?
+
 
 .code
-main proc
-	mov		eax,val1			; start eax with  10
+main proc ;
+  ;Set FIBNum number  F(0) = 1,  F(1) = 1                          
+	mov [ FIBNum ] ,1                                        ;  F(0) = 1
+	mov [ FIBNum +4] ,1                                      ;  F(1) = 1
+  
+  ;print f(0), f(1)
+	mov eax, 1
+	call WriteDec
+	call Crlf
+	call WriteDec
+	call Crlf
+	
+  ;
+	mov  esi, OFFSET FIBNum
+	mov ecx, 28                                              ; print FIBNum to  f(30)
+L1:
+	mov eax, [ esi ];
+	add eax, [ esi + 4];
+	mov [ esi + 8 ], eax;
+  
+	call WriteDec                                           ;print int 
+	call Crlf											   ;chang line
+	add esi,4
 
-	sub		eax,1				; subtract 1
-	sub		eax,2				; subtract 2
-	sub		eax,4				; subtract 4
-
-	call	DumpRegs			; display the registers
-
+	loop L1
+  
 	invoke ExitProcess,0
 main endp
 end main
